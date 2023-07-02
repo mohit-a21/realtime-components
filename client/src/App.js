cdf90a068ebaccf24d639dc8d9ccc83f14c16ad8
Mohit Agarwal <mohit.agarwal@drdroid.io>
2023-07-02 18:28:42 +0700

    Add link to realtime colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import React, {useEffect, useState} from "react";
+import {CopyToClipboard} from "react-copy-to-clipboard";
 import "./styles.css";
 import TextBox from "./TextBox";
 import axios from "axios";
@@ -16,6 +16,27 @@ const defaultColors = [
   "#f89e4f",
   "#ec015a"
 ];
+
+const linkGenerator = (endpoint, colors) => {
+  return `${endpoint}${colors.map(str => str.substring(1)).join("-")}`;
+}
+
+function generateContrastingColor(hexColor) {
+  // Remove the # symbol if present
+  hexColor = hexColor.replace("#", "");
+
+  // Convert the hex color to RGB format
+  const red = parseInt(hexColor.substring(0, 2), 16);
+  const green = parseInt(hexColor.substring(2, 4), 16);
+  const blue = parseInt(hexColor.substring(4, 6), 16);
+
+  // Calculate the relative luminance of the color
+  const luminance = (red * 0.299 + green * 0.587 + blue * 0.114) / 255;
+
+  // Determine the contrast color based on the luminance
+  return luminance > 0.5 ? "#000000": "#ffffff" ;
+}
+
 
 export default function App() {
   const [text, setText] = useState("");
@@ -43,6 +64,7 @@ export default function App() {
             response.data.secondary,
             response.data.accent
           ]);
+          setLoading(false);
         } catch (error) {
           console.error(error);
         }
@@ -54,6 +76,8 @@ export default function App() {
   const [background, setBackground] = useState("#071415");
   const [current, setCurrent] = useState(null);
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setCurrent(null);
@@ -62,13 +86,30 @@ export default function App() {
   }, [current]);
 
   return (
-    <div className="App" style={{ background: background }}>
+    <div className="App" style={{background: background}}>
       <div>
-        <TextBox onSubmit={setText} />
+        <h1 style={{color: generateContrastingColor(background)}}>
+          Add a text prompt to generate a color palette for the theme defined in the prompt
+        </h1>
       </div>
+      <div>
+        <TextBox onSubmit={
+          (t) => {
+            setText(t);
+            setLoading(true);
+          }
+        }/>
+      </div>
+      <div>
+        {
+          loading ? <h2 style={{color: generateContrastingColor(background)}}>Loading...</h2> : null
+        }
+      </div>
+
       <div className="container">
         {colors.map((color, index) => (
           <div key={index} className="card">
+
             <div
               style={{
                 background: color,
@@ -80,7 +121,7 @@ export default function App() {
             />
             <CopyToClipboard text={`color: ${color};`}>
               <p
-                style={{ color: color === background ? "#fff" : color }}
+                style={{color: color === background ? "#fff" : color}}
                 onClick={() => setCurrent(color)}
               >
                 {color}
@@ -88,6 +129,18 @@ export default function App() {
             </CopyToClipboard>
           </div>
         ))}
+      </div>
+      <div>
+        <a href={linkGenerator("https://realtimecolors.com/?colors=", colors)} target={"_blank"}>
+          <h2 style={{color: generateContrastingColor(background)}}>
+            Realtime Colors
+          </h2>
+        </a>
+        <a href={linkGenerator("https://coolors.co/", colors)} target={"_blank"}>
+          <h2 style={{color: generateContrastingColor(background)}}>
+            Coolors
+          </h2>
+        </a>
       </div>
     </div>
   );
--- models.py
+++ models.py
@@ -2,11 +2,12 @@ from pydantic import Field, BaseModel
 
 
 class ColorPallet(BaseModel):
-    text: str = Field(description="Color of text on the page which follows the theme in hexadecimal format.")
+    text: str = Field(description="Color of text should contrast from background color on the page which follows the theme in hexadecimal format.")
     background: str = Field(description="Background color of the page which follows the theme in hexadecimal format.")
     primary: str = Field(description="Primary color of the page which follows the theme in hexadecimal format.")
     secondary: str = Field(description="Secondary color of the page which follows the theme in hexadecimal format.")
     accent: str = Field(description="Accent of the theme of the page which follows the theme in hexadecimal format.")
+    reason: str = Field(description="Reason for the color pallet and how it follows the theme")
 
     def __str__(self):
         return f"Text: {self.text}\nBackground: {self.background}\nPrimary: {self.primary}\nSecondary: {self.secondary}\nAccent: {self.accent}"
